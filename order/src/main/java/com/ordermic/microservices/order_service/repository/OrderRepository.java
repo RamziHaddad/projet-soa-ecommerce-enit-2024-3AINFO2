74b70a0598384c1ed0605ac835d2ed9636826a30
haroun08 <129984358+haroun08@users.noreply.github.com>
2024-11-10 21:47:42 +0100

    Order

---
+++ order/src/main/java/com/ordermic/microservices/order_service/repository/OrderRepository.java
@@ -0,0 +1,6 @@
+package com.ordermic.microservices.order_service.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.ordermic.microservices.order_service.model.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+}
