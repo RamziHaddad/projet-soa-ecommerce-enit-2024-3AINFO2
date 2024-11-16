74dc19f01ca34d83ef9eea6c70c628ba1ce5aebd
haroun08 <129984358+haroun08@users.noreply.github.com>
2024-11-16 11:45:15 +0100

    Postgres conf

---
+++ order/src/main/java/com/microservices/order_service/service/OrderService.java
@@ -0,0 +1,28 @@
+package com.microservices.order_service.service;
+
+import com.microservices.order_service.dto.OrderRequest;
+import com.microservices.order_service.model.Order;
+import com.microservices.order_service.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    public void placeOrder(OrderRequest orderRequest){
+        Order order = new Order();
+
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setPrice(orderRequest.price());
+        order.setSkuCode(orderRequest.skuCode());
+        order.setQuantity(orderRequest.quantity());
+
+        orderRepository.save(order);
+
+
+    }
+}
--- order/src/main/java/com/ordermic/microservices/order_service/repository/OrderRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ordermic.microservices.order_service.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.ordermic.microservices.order_service.model.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long>{
-}
