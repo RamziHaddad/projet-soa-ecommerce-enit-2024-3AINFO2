cf7c5b3981f0dbf581171df2d1b5fbba5940be4a
DaoudAA <Ahmedaziz.daoud@gmail.com>
2024-11-18 19:25:43 +0100

    added pricing service

---
+++ catalog/catalog-service/src/main/java/org/ecommerce/service/PricingService.java
@@ -0,0 +1,20 @@
+package org.ecommerce.service;
+
+import java.util.UUID;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+
+@Path("/pricing")
+@RegisterRestClient(configKey="pricing-api")
+public interface PricingService {
+
+    @GET
+    @Path("/{id}")
+    public double getProductPrice(@PathParam("id") UUID id);
+
+}
--- catalog/catalog-service/src/main/java/org/ecommerce/service/ProductService.java
+++ catalog/catalog-service/src/main/java/org/ecommerce/service/ProductService.java
@@ -3,19 +3,23 @@ package org.ecommerce.service;
 import java.util.List;
 import java.util.UUID;
 
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
-
 import org.ecommerce.domain.Product;
 import org.ecommerce.exceptions.EntityAlreadyExistsException;
 import org.ecommerce.exceptions.EntityNotFoundException;
 import org.ecommerce.repository.ProductRepository;
+
+import io.quarkus.scheduler.Scheduled;
+import io.quarkus.scheduler.Scheduled.ConcurrentExecution;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class ProductService {
 
     @Inject
     ProductRepository productRepo;
+    @Inject
+    PricingService pricingService;
 
     public List<Product> findByRange(int page , int maxResults) {
         return productRepo.findByRange(page , maxResults);
@@ -30,7 +34,32 @@ public class ProductService {
     }
 
     public Product add(Product product, String categoryName) throws EntityAlreadyExistsException, EntityNotFoundException {
+        product.setBasePrice(pricingService.getProductPrice(product.getId()));
+        product.setShownPrice(product.getBasePrice());
         return productRepo.addProductWithCategory(product, categoryName);
+    }
+
+    @Scheduled(every = "12h", concurrentExecution = ConcurrentExecution.SKIP)
+    public void checkPriceUpdates(){
+        int page = 0;
+        int maxResults = 10; // Adjust the range as needed
+
+        List<Product> products;
+        do {
+            products = findByRange(page, maxResults);
+            products.forEach(product -> {
+                double newPrice = pricingService.getProductPrice(product.getId());
+                if (product.getShownPrice() != newPrice) {
+                    product.setShownPrice(newPrice);
+                    try {
+                        productRepo.update(product);
+                    } catch (EntityNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            page++;
+        } while (!products.isEmpty());
     }
 
     public Product updateProduct(Product product) throws EntityNotFoundException {
