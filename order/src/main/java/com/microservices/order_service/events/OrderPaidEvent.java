4273fe87b6fc12d18d64d2183f00be7eb4179ace
haroun08 <129984358+haroun08@users.noreply.github.com>
2024-12-26 23:36:37 +0100

    sync -> delivery , order

---
+++ order/src/main/java/com/microservices/order_service/events/OrderPaidEvent.java
@@ -0,0 +1,41 @@
+package com.microservices.order_service.events;
+
+import com.microservices.order_service.domain.PaymentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class OrderPaidEvent implements Serializable {
+    @Setter
+    private UUID orderId;
+    @Setter
+    private UUID addressId;
+    @Setter
+    private UUID cartId;
+    private LocalDateTime timestamp;
+    @Setter
+    private PaymentStatus paymentStatus;
+
+
+
+
+
+    @Override
+    public String toString() {
+        return "OrderPaidEvent{" +
+                "orderId=" + orderId +
+                ", addressId=" + addressId +
+                ", cartId=" + cartId +
+                ", timestamp=" + timestamp +
+                ", paymentStatus=" + paymentStatus +
+                '}';
+    }
+}
